ef3de304fcd0a9de3a55cab2bdaeb075027e204d
Zorn93 <olivier93170@hotmail.fr>
2015-10-28 17:05:29 +0100

    Ajout de type double

--- Projet.Niveau1.CalculatriceBasique/Program.cs
+++ Projet.Niveau1.CalculatriceBasique/Program.cs
@@ -6,7 +6,7 @@ namespace Projet.Niveau1.CalculatriceBasique
     {
         private static void Main(string[] args)
         {
-            Affichage.Afficher("Bonjour !");
+            Affichage.Afficher("Bonjour maître Anis !");
             TypeOperation();
         }
 
@@ -14,15 +14,20 @@ namespace Projet.Niveau1.CalculatriceBasique
         {
             Affichage.Afficher("Quelle type d'opération souhaitez vous faire :");
             Affichage.Afficher("1.Addition");
+            Affichage.Afficher("2.Soustraction");
+            Affichage.Afficher("3.Multiplication");
+            Affichage.Afficher("4.Division");
+
+
 
             int saisieClavier = Convert.ToInt16(Console.ReadLine());
 
             if (saisieClavier == 1)
             {
                 Affichage.AfficherSansRetourLigne("Premier nombre :");
-                int premierChiffre = Convert.ToInt16(Console.ReadLine());
+                double premierChiffre = Convert.ToDouble(Console.ReadLine());
                 Affichage.AfficherSansRetourLigne("Second nombre :");
-                int secondChiffre = Convert.ToInt16(Console.ReadLine());
+                double secondChiffre = Convert.ToDouble(Console.ReadLine());
 
                 var calcul = new Calcul(premierChiffre, secondChiffre);
                 var resultat = calcul.addition();
@@ -30,7 +35,46 @@ namespace Projet.Niveau1.CalculatriceBasique
                 Affichage.Afficher("Le résultat est : " + resultat);
                 TypeOperation();
             }
-            else
+            if (saisieClavier == 2)
+            {
+                Affichage.AfficherSansRetourLigne("Premier nombre :");
+                double premierChiffre = Convert.ToDouble(Console.ReadLine());
+                Affichage.AfficherSansRetourLigne("Second nombre :");
+                double secondChiffre = Convert.ToDouble(Console.ReadLine());
+
+                var calcul = new Calcul(premierChiffre, secondChiffre);
+                var resultat = calcul.soustraction();
+
+                Affichage.Afficher("Le résultat est : " + resultat);
+                TypeOperation();
+            }
+            if (saisieClavier == 3)
+            {
+                Affichage.AfficherSansRetourLigne("Premier nombre :");
+                double premierChiffre = Convert.ToDouble(Console.ReadLine());
+                Affichage.AfficherSansRetourLigne("Second nombre :");
+                double secondChiffre = Convert.ToDouble(Console.ReadLine());
+
+                var calcul = new Calcul(premierChiffre, secondChiffre);
+                var resultat = calcul.multiplication();
+
+                Affichage.Afficher("Le résultat est : " + resultat);
+                TypeOperation();
+            }
+            if (saisieClavier == 4)
+            {
+                Affichage.AfficherSansRetourLigne("Premier nombre :");
+                double premierChiffre = Convert.ToDouble(Console.ReadLine());
+                Affichage.AfficherSansRetourLigne("Second nombre :");
+                double secondChiffre = Convert.ToDouble(Console.ReadLine());
+
+                var calcul = new Calcul(premierChiffre, secondChiffre);
+                var resultat = calcul.division();
+
+                Affichage.Afficher("Le résultat est : " + resultat);
+                TypeOperation();
+            }
+
             {
                 Affichage.Afficher("Mauvaise saisie");
                 TypeOperation();
